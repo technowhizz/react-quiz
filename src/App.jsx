001f499966cca919959243b8d5d9394c0ce96b97
technowhizz <7688823+technowhizz@users.noreply.github.com>
2022-08-21 01:27:45 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Nav from "./components/Nav"
+
+export default function App(){
+    return(
+        <div className="app">
+            <Nav />
+            <h1>Test App</h1>
+        </div>
+    )
+}
