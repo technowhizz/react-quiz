8939ead5685406074457fb6e1d9f0c913a70a8bb
technowhizz <7688823+technowhizz@users.noreply.github.com>
2022-08-23 10:02:09 +0100

    Add recieve Questions from API and styling for answer selection and implement answer selection logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,104 @@
 import React from "react"
 import Welcome from "./components/Welcome"
 import Question from "./components/Question"
+import he from "he"
+import { nanoid } from "nanoid"
 
 export default function App(){
 
     const [welcome, setWelcome] = React.useState(true)
     const [questions, setQuestions] = React.useState()
+    const [questionObjects, setQuestionObjects] = React.useState()
+    const [questionElements, setQuestionElements] = React.useState()
+
 
     React.useEffect(() => {
-        fetch
-    })
+        fetch("https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple")
+            .then(response => response.json())
+            .then(responseData => setQuestions(responseData.results))
+    }, [])
+
+    React.useEffect(() => {
+        if (questions !== undefined){
+            const questionsArray = questions.map(obj => {
+                const correctAnswer = he.decode(obj.correct_answer)
+                const incorrectAnswers = obj.incorrect_answers.map(x => he.decode(x))
+                const answers = [correctAnswer, ...incorrectAnswers]
+                const id = nanoid()
+                const questionObj = {
+                    id: id,
+                    click: handleClick,
+                    question: he.decode(obj.question),
+                    answer: correctAnswer,
+                    answers: [
+                        {
+                            value: answers[0],
+                            isSelected: false
+                        },
+                        {
+                            value: answers[1],
+                            isSelected: false
+                        },
+                        {
+                            value: answers[2],
+                            isSelected: false
+                        },
+                        {
+                            value: answers[3],
+                            isSelected: false
+                        }
+                    ]
+                }
+                return(questionObj)
+                
+            })
+            setQuestionObjects(questionsArray)
+        } 
+    }, [questions])
+    
+    React.useEffect(() => {
+        if (questionObjects !== undefined){
+            const abc = questionObjects.map(obj => (
+                    <Question 
+                        key={obj.id} 
+                        data={obj}
+                    />                 
+                ))
+            setQuestionElements(abc)
+        }
+    },[questionObjects])
+
+    function handleClick(event, id){
+        setQuestionObjects((questionObjects) => {
+            const newQuestionObjs = questionObjects.map(obj => {
+                if(obj.id === id){
+                    const newAnswers = obj.answers.map((answer, index) => {
+                        if (index == event.target.id){
+                            return({
+                                ...answer,
+                                isSelected: !answer.isSelected
+                            })
+                        }else{
+                            return({
+                                ...answer,
+                                isSelected: false
+                            })
+                        }
+                    })
+                    return({
+                        ...obj,
+                        answers: newAnswers
+                    })
+                }else{
+                    return({...obj})
+                }
+            })
+            return newQuestionObjs
+        })
+        
+    
+
+    }
 
     function startClick(){
         setWelcome(false)
@@ -19,11 +108,8 @@ export default function App(){
         <div className={`app${welcome?"":" quizmode"}`}>
             <div className="app--container">
                 {welcome && <Welcome click={startClick} />}
-                {!welcome && <Question />}
-                {!welcome && <Question />}
-                {!welcome && <Question />}
-                {!welcome && <Question />}
-                {!welcome && <Question />}
+                {!welcome && questionElements}
+
             </div>
         </div>
     )
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,14 +1,23 @@
 import React from "react"
 
 export default function Question(props){
+    const {answer, answers, question, click, id} = {...props.data}
     return(
         <div className="question">
-            <h2 className="question--text">This is a question?</h2>
+            <h2 className="question--text">{question}</h2>
             <div className="question--answers">
-                <button className="question--answer-button">Hola</button>
-                <button className="question--answer-button">Answer 2</button>
-                <button className="question--answer-button">Answer 3</button>
-                <button className="question--answer-button">Answer 4</button>
+                <button className={`question--answer-button${answers[0].isSelected?" answer-selected":""}`} id={0} onClick={(event) => click(event, id)}>
+                    {answers[0].value}
+                </button>
+                <button className={`question--answer-button${answers[1].isSelected?" answer-selected":""}`} id={1} onClick={(event) => click(event, id)}>
+                    {answers[1].value}
+                </button>
+                <button className={`question--answer-button${answers[2].isSelected?" answer-selected":""}`} id={2} onClick={(event) => click(event, id)}>
+                    {answers[2].value}
+                </button>
+                <button className={`question--answer-button${answers[3].isSelected?" answer-selected":""}`} id={3} onClick={(event) => click(event, id)}>
+                    {answers[3].value}
+                </button>
             </div>
             <hr className="question--hr" />
         </div>
